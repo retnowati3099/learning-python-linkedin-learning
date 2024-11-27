c46409fe1e31fb596e6ea3d44a5354e67d417f00
Retno Wati <retnowati3099@gmail.com>
2024-11-27 15:52:06 +0700

    match-case

--- CH2-Basics/conditionals.py
+++ CH2-Basics/conditionals.py
@@ -14,5 +14,18 @@ def main():
     result = "x is less than y" if x < y else "x is greater or equal to y"
     print(result)
 
+    # match-case it easy to compare multiple values
+    value = "one"
+    match value:
+        case "one":
+            result = 1
+        case "two":
+            result = 2
+        case "three" | "four":
+            result = (3, 4)
+        case _:
+            result = -1
+    print(result)
+
 if __name__ == "__main__":
     main()

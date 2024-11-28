e8bedd5fba9470c2ff1d0f43f7e4e53bdb3c73a9
Retno Wati <retnowati3099@gmail.com>
2024-11-28 22:29:56 +0700

    modules

---
+++ CH2-Basics/modules.py
@@ -0,0 +1,11 @@
+# TODO: import the math module, which contains features for working with math
+import math
+
+# TODO:  the math module containes lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# TODO: in addition  to functions, some modules contain useful constants
+print("Pi is", math.pi)
+
+# TODO: try some of the math functions for yourself here
+print(math.pow(2, 3)) # output: 8.0

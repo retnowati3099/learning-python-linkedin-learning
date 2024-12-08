e2cc34cbc5cb61835c3b987ed25fe9b2abb5741a
retnowati3099 <retnowati3099@gmail.com>
2024-12-08 23:06:40 +0700

    reading and writing files

--- CH2-Basics/file.py
+++ CH2-Basics/file.py
@@ -1,13 +1,28 @@
 def main():
     # open a file for writing and create it if it doesn't exist
-    my_file = open("text.txt", "w+")
+    # my_file = open("text.txt", "w+")
+
+    # open the file for appending text to the end
+    # my_file = open("text.txt", "a+")
 
     # write some lines of data to the file
-    for i in range(10):
-        my_file.write("A line" + str(i) + "\n")
+    # for i in range(10):
+        # my_file.write("A line " + str(i + 1) + "\n")
+        # my_file.write("A new line\n")
 
     # close the file when done
-    my_file.close()
+    # my_file.close()
+
+    # open the file back up and read the contents
+    my_file = open("text.txt", "r")
+    if my_file.mode == "r":
+        # contents = my_file.read()
+        # print(contents)
+
+        # read the file line by line
+        fl = my_file.readlines()
+        for x in fl:
+            print(x)
 
 if __name__ == "__main__":
     main()

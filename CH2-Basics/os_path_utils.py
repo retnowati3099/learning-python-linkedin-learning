ddc779b01c317bd749807ca59006a9a3d093b95f
retnowati3099 <retnowati3099@gmail.com>
2024-12-15 20:52:26 +0700

    using library os and datetime

--- CH2-Basics/os_path_utils.py
+++ CH2-Basics/os_path_utils.py
@@ -13,5 +13,18 @@ def main():
     print("Item is a file:", path.isfile("text.txt"))
     print("Item is a directory:", path.isdir("text.txt"))
 
+    # work with file path
+    print("Item's path:", path.realpath("text.txt"))
+    print("Item's path and name:", path.split(path.realpath("text.txt")))
+
+    # get the modification time
+    t = time.ctime(path.getmtime("text.txt"))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime("text.txt")))
+
+    # calculate how long ago the item was modified
+    td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime("text.txt"))
+    print(td)
+
 if __name__ == "__main__":
     main()
